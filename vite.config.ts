c60a9bb0542e42fbe1bcc65dbf95dbd1bdc745c4
Kudadonbe <hussain.shareef@gmail.com>
2025-03-16 15:24:04 +0500

    Remove environment detection for production in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-/// Detect environment using `process.env.NODE_ENV`
-const isProduction = process.env.NODE_ENV === 'production'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
@@ -16,5 +13,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: isProduction ? '/SchoolSync/' : '/', // ✅ Ensures local dev works fine
 })
